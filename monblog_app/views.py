1ed8ceb25a7ff1c50f13b4df9de2de5938eac24f
Adem Nefzi <nefziadem100@gmail.com>
2026-06-15 14:28:12 +0100

    Update views.py

--- monblog_app/views.py
+++ monblog_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Article
+from .models import Article, Message
 
 # Page d'accueil : liste de tous les articles
 def liste_articles(request):
